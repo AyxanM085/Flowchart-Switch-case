a12d281e5b44c0542316bfd4bb028f8fd6b4a2b2
AyxanM085 <Aykhankhm@code.edu.az>
2024-05-15 13:50:33 +0400

    Add files via upload

---
+++ Flowchart-Switch-case/Task 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int num = 47;
+            bool flag = false;
+            if (num == 0 || num == 1)
+            {
+                Console.WriteLine("Neither prime , nor composite");
+            }
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    flag = true;
+                }
+
+            }
+            if (flag)
+            {
+                Console.WriteLine("Composite");
+            }
+            else
+            {
+                Console.WriteLine("Prime");
+            }
+        }
+    }
+}
+
+    
+       
+
+
+
+
+
+
+
+
+
+
+
---
+++ Flowchart-Switch-case/Task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    Console.Write("147 ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    int count = 0;
+                    while (n != 0)
+                    {
+                        count++;
+                        n /= 10;
+                    }
+
+                    Console.WriteLine("147 " + count);
+                }
+            }
+        }
+    }
+
+}
+    
+
